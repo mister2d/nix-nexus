dfc875e17954281718944811e911f0570496831d
mister2d <mister2d@users.noreply.github.com>
2026-03-23 02:26:34 -0400

    refactor(avina): remove dev-home module

--- hosts/avina/home.nix
+++ hosts/avina/home.nix
@@ -3,14 +3,7 @@
   imports = [
     ../../modules/user/bash.nix
     ../../modules/user/neovim-home.nix
-    ../../modules/user/dev-home.nix
   ];
-
-  programs.dev-home = {
-    enable = true;
-    enableMcpServers = false;
-    enableLlmAgents = false;
-  };
 
   home.stateVersion = "25.11";
 }
