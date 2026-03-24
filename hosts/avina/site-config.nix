3f9440dafe35b27198d71d9a145650ca00c7bb4f
mister2d <mister2d@users.noreply.github.com>
2026-03-24 01:43:49 -0400

    fix(avina): correct virtualization module config arg and site-config staging

---
+++ hosts/avina/site-config.nix
@@ -0,0 +1,8 @@
+{
+  matrixDomain = "matrix.novuscotia.com";
+  elementDomain = "element.novuscotia.com";
+  masDomain = "mas.novuscotia.com";
+  callDomain = "call.novuscotia.com";
+  coturnRealm = "turn.novuscotia.com";
+  vaultAddr = "https://vault.service.consul:8200";
+}
