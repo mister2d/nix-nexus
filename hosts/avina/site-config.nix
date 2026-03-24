7e6e3ca86dac838851f5fabcc80a91c1a00cb0e3
mister2d <mister2d@users.noreply.github.com>
2026-03-24 01:44:16 -0400

    fix(avina): remove site-config.nix accidentally staged by build test

--- hosts/avina/site-config.nix
+++
@@ -1,8 +0,0 @@
-{
-  matrixDomain = "matrix.novuscotia.com";
-  elementDomain = "element.novuscotia.com";
-  masDomain = "mas.novuscotia.com";
-  callDomain = "call.novuscotia.com";
-  coturnRealm = "turn.novuscotia.com";
-  vaultAddr = "https://vault.service.consul:8200";
-}
