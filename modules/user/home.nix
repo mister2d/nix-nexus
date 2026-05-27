06bdbe98e64e9834fabbd145bf3fe5491e246ce0
Dana D <mister2d@users.noreply.github.com>
2026-05-27 06:13:19 +0000

    fix: resolve evaluation warnings for proton-vpn and waybar systemd targets

--- modules/user/home.nix
+++ modules/user/home.nix
@@ -122,7 +122,7 @@ in
       # Secret Management & VPN
       libsecret # DBus interface for secrets (CLI: secret-tool)
       seahorse # GNOME GUI for managing keys and passwords
-      protonvpn-gui # Official ProtonVPN GTK client
+      proton-vpn # Official ProtonVPN GTK client
 
       # Yubikey & FIDO2 Tools
       yubikey-manager # GUI/CLI to manage YubiKeys
